4b803db7017bbe25f4c4a715a2fa7766a8f64e2c
rvinay88 <rvinay88@gmail.com>
2014-05-29 14:53:59 +0530

    Generate atoms, molecules and organisms

--- generator-neutrino/app/index.js
+++ generator-neutrino/app/index.js
@@ -38,11 +38,52 @@ var NeutrinoGenerator = yeoman.generators.Base.extend({
   },
 
   app: function () {
-    this.mkdir('app');
-    this.mkdir('app/scss');
-    this.mkdir('app/stylesheets');
-    this.mkdir('app/scripts');
-    this.mkdir('app/markup');
+    // Sass architecture
+    var atoms = ["grids", "type", "colors", "animations", "helpers"];
+      var helpers = ["functions", "mixins", "utilities"];
+
+    var molecules = ["headings", "links","tables", "forms", "buttons", "lists", "media", "progress", "labels", "badges", "wells", "dropdowns", "tooltips", ];
+        var files = ["variables","mixins","extends"];
+
+    var organisms = ["alerts", "button-groups", "breadcrumbs", "pagination", "tabs"];
+    var templates = ["header", "footer", "navigation", "article", "sidenav", "toolbar"];
+
+    this.mkdir('stylesheets');
+    this.mkdir('scripts');
+    this.mkdir('markup');
+    this.mkdir('images');
+    this.mkdir('scss');
+
+    //Generate manifest SCSS file
+    this.copy('style.scss', 'style.scss');
+
+    // Generate atoms
+    for(var i = 0; i < atoms.length; i++) {
+      this.mkdir('scss/atoms/'+atoms[i]);
+      if(atoms[i]!='helpers') {
+        this.copy('_empty.scss', 'scss/atoms/'+atoms[i]+'/_'+atoms[i]+'.scss');
+      }
+    }
+
+    //Generate atom helpers
+    for(var i = 0; i < helpers.length; i++) {
+      this.mkdir('scss/atoms/helpers/'+helpers[i]);
+      this.copy('_empty.scss', 'scss/atoms/helpers/'+helpers[i]+'/_'+helpers[i]+'.scss');
+    }
+
+    //Generate molecules and internal files
+    for(var i = 0; i < molecules.length; i++) {
+      this.mkdir('scss/molecules/'+molecules[i]);
+      this.copy('_variables.scss','scss/molecules/'+molecules[i]+'/_variables.scss');
+      this.copy('_mixins.scss','scss/molecules/'+molecules[i]+'/_mixins.scss');
+      this.copy('_extends.scss','scss/molecules/'+molecules[i]+'/_extends.scss');
+      this.copy('_template.scss','scss/molecules/'+molecules[i]+'/_'+molecules[i]+'.scss');
+    }
+
+    //Generate Organisms and internal files
+    for(var i = 0; i < organisms.length; i++) {
+      this.mkdir('scss/organisms/'+organisms[i]);
+    }
 
     this.copy('_package.json', 'package.json');
     this.copy('_bower.json', 'bower.json');
